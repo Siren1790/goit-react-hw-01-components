b37be20405e91354fb3af96cc92fbc11a1d04889
Sergii Karpus <49605599+Siren1790@users.noreply.github.com>
2023-02-12 14:29:34 +0200

    Format Prettier

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -1,30 +1,35 @@
-import css from 'Components/FriendList/FriendList.module.css'
+import css from 'Components/FriendList/FriendList.module.css';
 import PropTypes from 'prop-types';
 
-const FriendList = ({friends}) => {
-    return (
-        <ul className={css["friend-list"]}>
-            {friends.map(({id, isOnline, avatar, name}) => {
-                return (
-                    <li key={id} className={css.item}>
-                        <span className={isOnline ? css.online : css.offline}></span>
-                        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-                        <p className={css.name}>{name}</p>
-                  </li>  
-                )
-            })}
-        </ul>
-    )
-}
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css['friend-list']}>
+      {friends.map(({ id, isOnline, avatar, name }) => {
+        return (
+          <li key={id} className={css.item}>
+            <span className={isOnline ? css.online : css.offline}></span>
+            <img
+              className={css.avatar}
+              src={avatar}
+              alt="User avatar"
+              width="48"
+            />
+            <p className={css.name}>{name}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-            avatar: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            isOnline: PropTypes.bool.isRequired,
-            id: PropTypes.number.isRequired,
-        }),
-    ),
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
 };
 export default FriendList;
--- src/Components/Transactions/TransactionHistory.jsx
+++ src/Components/Transactions/TransactionHistory.jsx
@@ -1,40 +1,40 @@
-import css from "Components/Transactions/Transactions.module.css";
+import css from 'Components/Transactions/Transactions.module.css';
 import PropTypes from 'prop-types';
 
 const TransactionHistory = ({ transactions }) => {
-    return (
-        <table className={css["transaction-history"]}>
-            <caption>Transaction history</caption>
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                </tr>
-            </thead>
-            <tbody>
-            {transactions.map(({id, type, amount, currency}) => {
-                return (
-                    <tr key={id}>
-                        <td>{type}</td>
-                        <td>{amount}</td>
-                        <td>{currency}</td>
-                    </tr>
-                )
-            })}
-            </tbody>
-        </table>
-    )
-}
+  return (
+    <table className={css['transaction-history']}>
+      <caption>Transaction history</caption>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 TransactionHistory.protoTypes = {
-    transactions: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            type: PropTypes.string.isRequired,
-            amount: PropTypes.string.isRequired,
-            currency: PropTypes.string.isRequired,
-        }),
-    ),
-}
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
 export default TransactionHistory;
